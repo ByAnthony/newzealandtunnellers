552d60fdbee7b301d8c935708e21375adc8bf0a1
ByAnthony <anthony.byledbal@icloud.com>
2024-06-12 21:22:13 +0100

    extract rollquery

--- app/api/tunnellers/route.ts
+++ app/api/tunnellers/route.ts
@@ -1,20 +1,12 @@
 import { NextResponse } from "next/server";
 import { mysqlConnection } from "../../utils/api/mysqlConnection";
-import { Tunneller } from "../../types/roll";
+import { rollQuery } from "../../utils/api/queries/rollQuery";
 
 export async function GET() {
     const connection = await mysqlConnection();
 
     try {
-        const query = `SELECT t.id
-        , t.surname
-        , t.forename
-        , DATE_FORMAT(t.birth_date, '%Y') AS birthDate
-        , DATE_FORMAT(t.death_date, '%Y') AS deathDate 
-        
-        FROM tunneller t ORDER BY t.surname, t.forename ASC`;
-
-        const [results]: Array<any> = await connection.query(query);
+        const [results]: Array<any> = await rollQuery(connection);
 
         const modifiedResults = results.map((result: any) => ({
           ...result,
