9b1418aea7ab963a729a614ceaa9151ff6875589
ByAnthony <anthony.byledbal@icloud.com>
2024-06-12 19:39:29 +0100

    grouped by surname

--- app/api/tunnellers/route.ts
+++ app/api/tunnellers/route.ts
@@ -2,14 +2,6 @@ import { NextResponse } from "next/server";
 import { mysqlConnection } from "../../utils/api/mysqlConnection";
 import { Tunneller } from "../../types/roll";
 
-type DatabaseData = {
-  id: number,
-  surname: string,
-  forename: string,
-  birth_date: string | null,
-  death_date: string | null,
-}
-
 export async function GET() {
     const connection = await mysqlConnection();
 
@@ -24,24 +16,12 @@ export async function GET() {
 
         const [results]: Array<any> = await connection.query(query);
 
-        const groupedData: Record<string, Tunneller[]> = results.reduce((acc: Record<string, Tunneller[]>, tunneller: DatabaseData) => {
-          const firstLetter = tunneller.surname.charAt(0).toUpperCase();
-          if (!acc[firstLetter]) {
-            acc[firstLetter] = [];
-          }
-          acc[firstLetter].push({
-            id: tunneller.id,
-            name: {
-              surname: tunneller.surname,
-              forename: tunneller.forename
-            },
-            birthDate: tunneller.birth_date,
-            deathDate: tunneller.death_date
-          });
-          return acc;
-        }, {} as { [key: string]: Tunneller[] });
+        const modifiedResults = results.map((result: any) => ({
+          ...result,
+          fullName: `${result.forename} ${result.surname}`,
+        }));
         
-        return NextResponse.json(groupedData)
+        return NextResponse.json(modifiedResults)
     } catch (error) {
         return NextResponse.json({
             error: error
