1e6f2c526b306bc8eb98731ff976ca2bc2c924dc
ByAnthony <anthony.byledbal@icloud.com>
2024-06-16 10:50:57 +0100

    tidy up homepage

--- app/components/HomePage/HistoryChapters/HistoryChapters.tsx
+++ app/components/HomePage/HistoryChapters/HistoryChapters.tsx
@@ -1,11 +1,11 @@
-import { useRef, useState } from 'react';
-import { Articles } from '../../../types/article';
+import { useRef, useState } from "react";
+import { historyChapter } from "../../../types/homepage";
 
-import STYLES from './HistoryChapters.module.scss';
+import STYLES from "./HistoryChapters.module.scss";
 
 type Props = {
-    articles: Articles[]
-  }
+  articles: historyChapter[];
+};
 
 export function HistoryChapters({ articles }: Props) {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -49,47 +49,78 @@ export function HistoryChapters({ articles }: Props) {
   const isLastCard = currentIndex === articles.length - 3;
   const isFirstCard = currentIndex === 0;
 
-  const buttonDisabledStyle = { color: 'rgb(44, 46, 47)', cursor: 'not-allowed' };
+  const buttonDisabledStyle = {
+    color: "rgb(44, 46, 47)",
+    cursor: "not-allowed",
+  };
   const buttonFirstCard = isFirstCard ? buttonDisabledStyle : {};
   const buttonLastCard = isLastCard ? buttonDisabledStyle : {};
 
   return (
-    <div className={STYLES['history-chapter']}>
-      <div id="history" className={STYLES['chapter-cards-wrapper']}>
-        <div className={STYLES['chapter-cards-menu']}>
-          <h3>
-            History of the Company
-          </h3>
-          <div className={STYLES['chapter-cards-nav']}>
-            <button type="button" onClick={handleScrollLeft} disabled={isFirstCard} style={buttonFirstCard} aria-label="Scroll to the left to see previous chapters">&larr;</button>
-            <button type="button" onClick={handleScrollRight} disabled={isLastCard} style={buttonLastCard} aria-label="Scroll to the right to see next chapters">&rarr;</button>
+    <div className={STYLES["history-chapter"]}>
+      <div id="history" className={STYLES["chapter-cards-wrapper"]}>
+        <div className={STYLES["chapter-cards-menu"]}>
+          <h3>History of the Company</h3>
+          <div className={STYLES["chapter-cards-nav"]}>
+            <button
+              type="button"
+              onClick={handleScrollLeft}
+              disabled={isFirstCard}
+              style={buttonFirstCard}
+              aria-label="Scroll to the left to see previous chapters"
+            >
+              &larr;
+            </button>
+            <button
+              type="button"
+              onClick={handleScrollRight}
+              disabled={isLastCard}
+              style={buttonLastCard}
+              aria-label="Scroll to the right to see next chapters"
+            >
+              &rarr;
+            </button>
           </div>
         </div>
-        <div className={STYLES['chapter-cards']} ref={containerRef} onScroll={handleScroll}>
+        <div
+          className={STYLES["chapter-cards"]}
+          ref={containerRef}
+          onScroll={handleScroll}
+        >
           {articles.map((article) => {
             const divStyle = {
               backgroundImage: `url(../images/history/${article.image})`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center center',
+              backgroundSize: "cover",
+              backgroundPosition: "center center",
             };
             const splitTitle = (string: string) => {
-              const split = string.split('\\');
+              const split = string.split("\\");
               return split;
             };
 
             return (
-              <div className={STYLES['chapter-card']} key={articles.indexOf(article)} style={divStyle}>
+              <div
+                className={STYLES["chapter-card"]}
+                key={articles.indexOf(article)}
+                style={divStyle}
+              >
                 <a
-                  href={`/history/${article.url}`}
-                  className={STYLES['link-button']}
-                  aria-label={`Go to Chapter ${article.chapter}: ${article.title.replace(/\\/g, ' ')}`}
+                  href={`/history/${article.id}`}
+                  className={STYLES["link-button"]}
+                  aria-label={`Go to Chapter ${article.chapter}: ${article.title.replace(/\\/g, " ")}`}
                 >
-                  <div className={STYLES['chapter-card-dimmer']}>
-                    <div className={STYLES['chapter-card-content']}>
+                  <div className={STYLES["chapter-card-dimmer"]}>
+                    <div className={STYLES["chapter-card-content"]}>
                       <div>
-                        <span className={STYLES['title-line-1']}>{ splitTitle(article.title)[0] }</span>
-                        <span className={STYLES['title-line-2']}>{ splitTitle(article.title)[1] }</span>
-                        <span className={STYLES['title-line-3']}>{`Chapter ${article.chapter}`}</span>
+                        <span className={STYLES["title-line-1"]}>
+                          {splitTitle(article.title)[0]}
+                        </span>
+                        <span className={STYLES["title-line-2"]}>
+                          {splitTitle(article.title)[1]}
+                        </span>
+                        <span
+                          className={STYLES["title-line-3"]}
+                        >{`Chapter ${article.chapter}`}</span>
                       </div>
                     </div>
                   </div>
