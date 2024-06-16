e3ea6d78a96d9846878f2dcd654dad1b6f94c9cb
ByAnthony <anthony.byledbal@icloud.com>
2024-06-16 06:53:06 +0100

    remove console log

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,6 @@ export default async function Home() {
         historyImageChapters,
       ),
     };
-    console.log(homepage);
 
     return <HomePage homepage={homepage} />;
   } catch (error) {
