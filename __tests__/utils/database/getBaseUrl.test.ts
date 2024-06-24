1ad63ceafec923458aff38f26b969053a47d7cf9
ByAnthony <anthony.byledbal@icloud.com>
2024-06-24 21:02:51 +0100

    add test for database

---
+++ __tests__/utils/database/getBaseUrl.test.ts
@@ -0,0 +1,16 @@
+import { getBaseUrl } from "../../../app/utils/database/getBaseUrl";
+
+describe("getBaseUrl function", () => {
+  test.each([
+    ["production", "https://www.nztunnellers.com"],
+    ["development", "http://localhost:3000"],
+    [undefined, "http://localhost:3000"],
+  ])(
+    'returns the localhost URL when VERCEL_ENV is "%s"',
+    (vercelEnv, expectedUrl) => {
+      process.env.VERCEL_ENV = vercelEnv;
+      const result = getBaseUrl();
+      expect(result).toBe(expectedUrl);
+    },
+  );
+});
