86588bc7d9aec8daafdd48e6efe059bc6931aaf3
ByAnthony <anthony.byledbal@icloud.com>
2024-06-27 12:20:47 +0100

    add test for profile diary

---
+++ __tests__/components/Profile/DiaryDied.test.tsx
@@ -0,0 +1,66 @@
+import { render, screen } from "@testing-library/react";
+import { mockDate, mockDeath } from "../../mocks/mockTunneller";
+import { DiaryDied } from "../../../app/components/Profile/ProfileDiary/DiaryDied/DiaryDied";
+import { mockAfterWarDeath } from "../../mocks/mockDeath";
+
+describe("Death War", () => {
+  const component = <DiaryDied death={mockDeath} />;
+
+  test("renders the component correctly", () => {
+    const { asFragment } = render(component);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test("renders age and date when known", () => {
+    render(component);
+
+    expect(screen.getByText("Died at the age of 89")).toBeInTheDocument();
+    expect(screen.getByText("13 October 1926")).toBeInTheDocument();
+  });
+
+  test("renders date when age unknown", () => {
+    const mockComponent = (
+      <DiaryDied death={{ ...mockDeath, ageAtDeath: null }} />
+    );
+
+    render(mockComponent);
+
+    expect(screen.queryByText("Died at the age of 89")).not.toBeInTheDocument();
+    expect(screen.getByText("13 October 1926")).toBeInTheDocument();
+  });
+});
+
+describe("Post War Death", () => {
+  const component = <DiaryDied death={mockAfterWarDeath} />;
+
+  test("renders the component correctly", () => {
+    const { asFragment } = render(component);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test("renders age and date when known", () => {
+    render(component);
+
+    expect(screen.getByText("Died at the age of 89")).toBeInTheDocument();
+    expect(screen.getByText("13 October 1926")).toBeInTheDocument();
+  });
+
+  test("renders date when age unknown", () => {
+    const mockComponent = (
+      <DiaryDied death={{ ...mockAfterWarDeath, ageAtDeath: null }} />
+    );
+
+    render(mockComponent);
+
+    expect(screen.queryByText("Died at the age of 89")).not.toBeInTheDocument();
+    expect(screen.getByText("13 October 1926")).toBeInTheDocument();
+  });
+});
+
+test("does not render the component when unknown", () => {
+  const { container } = render(<DiaryDied death={null} />);
+
+  expect(container).toBeEmptyDOMElement();
+});
