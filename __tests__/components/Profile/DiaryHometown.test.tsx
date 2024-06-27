2c4f63fa1860ed15d826a09f09cbecfd9af20a8d
ByAnthony <anthony.byledbal@icloud.com>
2024-06-27 12:27:26 +0100

    add test for profile diary

---
+++ __tests__/components/Profile/DiaryHometown.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import { DiaryHometown } from "../../../app/components/Profile/ProfileDiary/DiaryHometown/DiaryHometown";
+
+const component = <DiaryHometown residence="Wellington" />;
+
+test("renders the component correctly", () => {
+  const { asFragment } = render(component);
+
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test("renders residence when known", () => {
+  render(component);
+
+  expect(screen.getByText("Live")).toBeInTheDocument();
+  expect(screen.getByText("Hometown")).toBeInTheDocument();
+  expect(screen.getByText("Wellington")).toBeInTheDocument();
+});
+
+test("does not render residence when unknown", () => {
+  const { container } = render(<DiaryHometown residence={null} />);
+
+  expect(container).toBeEmptyDOMElement();
+});
