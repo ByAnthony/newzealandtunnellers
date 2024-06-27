399ee97a911b217d40a8156e782cd1e744052c4b
ByAnthony <anthony.byledbal@icloud.com>
2024-06-27 04:12:15 +0100

    add tests for footer

---
+++ __tests__/components/Footer/Footer.test.tsx
@@ -0,0 +1,20 @@
+import "@testing-library/jest-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Footer } from "../../../app/components/Footer/Footer";
+
+const component = <Footer />;
+
+test("renders the component correctly", () => {
+  const { asFragment } = render(component);
+
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test("can scroll to top of the page", () => {
+  global.scrollTo = jest.fn();
+
+  render(component);
+  fireEvent.click(screen.getByRole("button"));
+
+  expect(global.scrollTo).toHaveBeenCalledWith(0, 0);
+});
