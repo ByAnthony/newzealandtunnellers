e52d4419bbb1514d1aed728376d7e08847242986
ByAnthony <anthony.byledbal@icloud.com>
2024-06-27 15:24:16 +0100

    add test for timeline and fix disease event

--- __tests__/components/Timeline/TimelineEvents/TimelineEvents.test.tsx
+++ __tests__/components/Timeline/TimelineEvents/TimelineEvents.test.tsx
@@ -1,7 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import { TimelineEvents } from "../../../../app/components/Timeline/TimelineEvents/TimelineEvents";
-import { mockDeath, mockMilitaryYears } from "../../../mocks/mockTunneller";
-import { mockFrontEventsWithKilledInAction } from "../../../mocks/mockFrontEvents";
+import {
+  mockDeath,
+  mockEndOfService,
+  mockMilitaryYears,
+} from "../../../mocks/mockTunneller";
+import {
+  mockFrontEventsWithDiedOfDisease,
+  mockFrontEventsWithKilledInAction,
+} from "../../../mocks/mockFrontEvents";
 
 test("should render TimelineEvents", () => {
   const { asFragment } = render(
@@ -11,7 +18,7 @@ test("should render TimelineEvents", () => {
   expect(asFragment()).toMatchSnapshot();
 });
 
-describe("TimelineEvents place()", () => {
+describe("TimelineEvents", () => {
   const makeMockDeath = (location: string, town: string) => ({
     ...mockDeath,
     place: {
@@ -19,6 +26,44 @@ describe("TimelineEvents place()", () => {
       location,
       town,
     },
+  });
+
+  test("should render disease", () => {
+    render(
+      <TimelineEvents
+        militaryYears={{
+          ...mockMilitaryYears,
+          frontEvents: mockFrontEventsWithDiedOfDisease,
+          endOfService: { ...mockEndOfService, deathWar: true },
+        }}
+        death={{
+          ...mockDeath,
+          date: { year: "1917", dayMonth: "8 August" },
+          cause: { cause: "Died of disease", circumstances: "Pneumonia" },
+        }}
+      />,
+    );
+
+    expect(screen.getByText("(Pneumonia)")).toBeInTheDocument();
+  });
+
+  test("should render injuries", () => {
+    render(
+      <TimelineEvents
+        militaryYears={{
+          ...mockMilitaryYears,
+          frontEvents: mockFrontEventsWithDiedOfDisease,
+          endOfService: { ...mockEndOfService, deathWar: false },
+        }}
+        death={{
+          ...mockDeath,
+          date: { year: "1917", dayMonth: "8 August" },
+          cause: { cause: "Died of disease", circumstances: "Pneumonia" },
+        }}
+      />,
+    );
+
+    expect(screen.getByText("Pneumonia")).toBeInTheDocument();
   });
 
   test("should render location and town when known", () => {
