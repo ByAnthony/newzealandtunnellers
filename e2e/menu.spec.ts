02be44aab4b780a83cc0c95859733a94849176ca
Anthony Byledbal <anthony.byledbal@skyscanner.net>
2024-06-28 16:20:15 +0100

    add e2e test for menu

---
+++ e2e/menu.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from "@playwright/test";
+
+test("can search for a tunneller", async ({ page }) => {
+  await page.goto("/tunnellers");
+
+  const search = await page.locator("input");
+  expect(search).toHaveAttribute("placeholder", "Search for a Tunneller");
+
+  const input = "james williamson";
+
+  expect(search.inputValue()).toBeNull;
+  await search.type(input);
+  expect(await search.inputValue()).toEqual(input);
+
+  const names = await page
+    .getByRole("link", { name: "See James Williamson profile" })
+    .evaluateAll((elements) => {
+        return elements.map((element) => element.textContent!);
+    });
+
+  console.log(names);
+  expect(names).toHaveLength(2);
+});
