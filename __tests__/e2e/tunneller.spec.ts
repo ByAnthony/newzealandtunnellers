ad8d015f5752606eabe2055935c37ce8884acc39
ByAnthony <anthony.byledbal@icloud.com>
2024-06-30 11:12:05 +0100

    fix too many connection issue

--- __tests__/e2e/tunneller.spec.ts
+++ __tests__/e2e/tunneller.spec.ts
@@ -1,5 +1,14 @@
 import { test, expect } from "@playwright/test";
 
+test("can click on Tunnellers to go to Tunnellers list", async ({ page }) => {
+  await page.goto("/tunnellers/274");
+
+  page.getByRole("link", { name: "Tunnellers" }).first().click();
+  await page.waitForLoadState("domcontentloaded");
+
+  await expect(page).toHaveURL(/tunnellers/);
+});
+
 test("can click on timeline button", async ({ page }) => {
   await page.goto("/tunnellers/274");
 
@@ -11,7 +20,7 @@ test("can click on awmm link", async ({ page }) => {
   await page.goto("/tunnellers/274");
 
   page.getByRole("link", { name: "Online Cenotaph He Toa" }).click();
-  await page.waitForLoadState("domcontentloaded", { timeout: 10000 });
+  await page.waitForLoadState("domcontentloaded", { timeout: 15000 });
 
   await expect(page).toHaveURL(
     "https://www.aucklandmuseum.com/war-memorial/online-cenotaph/record/C43340",
--- __tests__/utils/database/mysqlConnection.test.ts
+++ __tests__/utils/database/mysqlConnection.test.ts
@@ -1,31 +1,28 @@
-import { Connection } from "node_modules/mysql2/typings/mysql/lib/Connection";
 import { mysqlConnection } from "../../../app/utils/database/mysqlConnection";
-import * as mysql from "mysql2/promise";
-import { Prepare } from "node_modules/mysql2/typings/mysql/lib/protocol/sequences/Prepare";
+import mysql from "mysql2/promise";
 
-jest.mock("mysql2/promise");
+// Mock the mysql2/promise module
+jest.mock("mysql2/promise", () => ({
+  createPool: jest.fn(),
+}));
 
-describe("mysqlConnection function", () => {
-  test("creates a MySQL connection with the correct environment variables", async () => {
+describe("mysqlConnection", () => {
+  it("creates a MySQL pool with the correct configuration", () => {
     process.env.MYSQL_HOST = "localhost";
     process.env.MYSQL_USER = "user";
     process.env.MYSQL_PASSWORD = "password";
     process.env.MYSQL_DATABASE = "database";
     process.env.MYSQL_PORT = "3306";
 
-    const mockConnection = {} as any;
-    jest.spyOn(mysql, "createConnection").mockResolvedValue(mockConnection);
+    mysqlConnection();
 
-    const connection = await mysqlConnection();
-
-    expect(mysql.createConnection).toHaveBeenCalledWith({
+    expect(mysql.createPool).toHaveBeenCalledWith({
+      connectionLimit: 10,
       host: "localhost",
       user: "user",
       password: "password",
       database: "database",
       port: 3306,
     });
-
-    expect(connection).toEqual(mockConnection);
   });
 });
