f855279d06b61385b5dfcb2a777ed47122a25d51
ByAnthony <anthony.byledbal@icloud.com>
2024-07-02 18:55:03 +0100

    gha connect db

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -12,26 +12,25 @@ import {
   aboutUsTitle,
 } from "@/utils/database/queries/aboutUsQuery";
 
-async function fetchPage() {
-  const connection = mysqlConnection();
-
-  const data: AboutUsData = await aboutUsTitle(connection);
-  const sections: SectionData[] = await aboutUsSections(connection);
-  const images: ImageData[] = await aboutUsImage(connection);
-
-  const article: AboutUsArticle = {
-    id: data.id,
-    title: data.title,
-    section: sections,
-    image: images,
-  };
-
-  connection.end();
-  return article;
-}
-
 export default async function About() {
-  const article = await fetchPage();
+  try {
+    const connection = mysqlConnection();
 
-  return <AboutUs article={article} />;
+    const data: AboutUsData = await aboutUsTitle(connection);
+    const sections: SectionData[] = await aboutUsSections(connection);
+    const images: ImageData[] = await aboutUsImage(connection);
+
+    const article: AboutUsArticle = {
+      id: data.id,
+      title: data.title,
+      section: sections,
+      image: images,
+    };
+
+    connection.end();
+
+    return <AboutUs article={article} />;
+  } catch (error) {
+    return { error: error };
+  }
 }
