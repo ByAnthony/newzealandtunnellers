5e4f19ba17fcce748bb65f7b02be08814283ae05
ByAnthony <anthony.byledbal@icloud.com>
2024-07-02 18:42:24 +0100

    refactor aboutus

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -12,25 +12,26 @@ import {
   aboutUsTitle,
 } from "@/utils/database/queries/aboutUsQuery";
 
+async function fetchPage() {
+  const connection = mysqlConnection();
+
+  const data: AboutUsData = await aboutUsTitle(connection);
+  const sections: SectionData[] = await aboutUsSections(connection);
+  const images: ImageData[] = await aboutUsImage(connection);
+
+  const article: AboutUsArticle = {
+    id: data.id,
+    title: data.title,
+    section: sections,
+    image: images,
+  };
+
+  connection.end();
+  return article;
+}
+
 export default async function About() {
-  try {
-    const connection = mysqlConnection();
+  const article = await fetchPage();
 
-    const data: AboutUsData = await aboutUsTitle(connection);
-    const sections: SectionData[] = await aboutUsSections(connection);
-    const images: ImageData[] = await aboutUsImage(connection);
-
-    const article: AboutUsArticle = {
-      id: data.id,
-      title: data.title,
-      section: sections,
-      image: images,
-    };
-
-    connection.end();
-
-    return <AboutUs article={article} />;
-  } catch (error) {
-    return { error: error };
-  }
+  return <AboutUs article={article} />;
 }
