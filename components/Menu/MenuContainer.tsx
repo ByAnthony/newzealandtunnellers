d0bb5cd177845689d2bda8335b69de619694d1bf
ByAnthony <anthony.byledbal@icloud.com>
2024-07-06 07:12:54 +0100

    add type for menu container

--- components/Menu/MenuContainer.tsx
+++ components/Menu/MenuContainer.tsx
@@ -4,9 +4,10 @@ import { useEffect, useState } from "react";
 
 import { getTunnellers } from "@/utils/database/getEndpoint";
 import { Menu } from "./Menu";
+import { TunnellerWithFullNameData } from "@/types/tunnellers";
 
 export async function MenuContainer() {
-  const [tunnellers, setTunnellers] = useState([]);
+  const [tunnellers, setTunnellers] = useState<TunnellerWithFullNameData[]>([]);
 
   useEffect(() => {
     getTunnellers().then((tunnellers) => {
