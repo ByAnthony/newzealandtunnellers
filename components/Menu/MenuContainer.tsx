05748f950da0fd354e1a8ded12ffae4e1de4c856
ByAnthony <anthony.byledbal@icloud.com>
2024-07-08 07:45:24 +0100

    client side for menu

--- components/Menu/MenuContainer.tsx
+++ components/Menu/MenuContainer.tsx
@@ -1,8 +1,19 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 import { getTunnellers } from "@/utils/database/getEndpoint";
 import { Menu } from "./Menu";
+import { TunnellerWithFullNameData } from "@/types/tunnellers";
 
 export async function MenuContainer() {
-  const tunnellers = await getTunnellers();
+  const [tunnellers, setTunnellers] = useState<TunnellerWithFullNameData[]>([]);
+
+  useEffect(() => {
+    getTunnellers().then((tunnellers) => {
+      setTunnellers(tunnellers);
+    });
+  }, []);
 
   return <Menu tunnellers={tunnellers} />;
 }
