086e751896b5b5fe9ee7e7340c54b204525e2496
Anthony Byledbal <anthony.byledbal@skyscanner.net>
2024-07-24 15:07:01 +0100

    add transition component

--- components/Transition/Transition.tsx
+++ components/Transition/Transition.tsx
@@ -1,13 +1,17 @@
-"use client"
+"use client";
 
 import { usePathname } from "next/navigation";
 
-export default function Transition({children}: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    const pathname = usePathname();
-    
-    return (
-        <div key={pathname} className="fade-in">{children}</div>
-    )
+export default function Transition({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const pathname = usePathname();
+
+  return (
+    <div key={pathname} className="fade-in">
+      {children}
+    </div>
+  );
 }
