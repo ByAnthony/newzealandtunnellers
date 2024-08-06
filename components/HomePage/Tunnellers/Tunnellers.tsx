e299b89bc4cb2e4b1ebe3c210e24687abf01c59a
ByAnthony <anthony.byledbal@icloud.com>
2024-08-06 22:04:55 +0100

    add link to tunnellers

--- components/HomePage/Tunnellers/Tunnellers.tsx
+++ components/HomePage/Tunnellers/Tunnellers.tsx
@@ -1,4 +1,4 @@
-// import Link from "next/link";
+import Link from "next/link";
 
 import STYLES from "./Tunnellers.module.scss";
 
@@ -8,12 +8,11 @@ export function Tunnellers() {
       <div className={STYLES.intro}>
         <h1>The Kiwis who fought beneath the no man&rsquo;s land</h1>
       </div>
-      {/* <Link href="/tunnellers/" className={STYLES["link-roll"]}>
-        <div className={STYLES.roll}>
-          <p>Discover</p>
-          <span>The Tunnellers</span>
-        </div>
-      </Link> */}
+      <div className={STYLES["roll-container"]}>
+        <Link href="/tunnellers/" className={STYLES.roll}>
+          The New Zealand Tunnellers
+        </Link>
+      </div>
     </>
   );
 }
