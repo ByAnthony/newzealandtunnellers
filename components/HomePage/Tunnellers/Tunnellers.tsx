e80979d8ce4ec50811bb69ae72464c37480be43e
ByAnthony <anthony.byledbal@icloud.com>
2024-08-07 19:19:48 +0100

    refactor link to tunnellers on homepage

--- components/HomePage/Tunnellers/Tunnellers.tsx
+++ components/HomePage/Tunnellers/Tunnellers.tsx
@@ -8,10 +8,13 @@ export function Tunnellers() {
       <div className={STYLES.intro}>
         <h1>The Kiwis who fought beneath the no man&rsquo;s land</h1>
       </div>
-      <div className={STYLES["roll-container"]}>
-        <Link href="/tunnellers/" className={STYLES.roll}>
-          The New Zealand Tunnellers
-        </Link>
+      <div className={STYLES["roll-wrapper"]}>
+        <div className={STYLES["roll-container"]}>
+          <Link href="/tunnellers/" className={STYLES.roll}>
+            <p className={STYLES.discover}>Discover</p>
+            The New Zealand Tunnellers
+          </Link>
+        </div>
       </div>
     </>
   );
