3b7af2663f5479612dd6bae87be5ff0f2ec7ff47
ByAnthony <anthony.byledbal@icloud.com>
2024-08-13 21:39:43 +0100

    add tests for tunnellers on homepage

--- __tests__/unit/components/Homepage/Tunnellers/Tunnellers.test.tsx
+++ __tests__/unit/components/Homepage/Tunnellers/Tunnellers.test.tsx
@@ -1,20 +1,75 @@
 import { render, screen } from "@testing-library/react";
+import React from "react";
 
 import { Tunnellers } from "@/components/HomePage/Tunnellers/Tunnellers";
+import * as useWindowDimensionsHook from "@/utils/helpers/useWindowDimensions";
 
-describe("Tunnellers", () => {
-  test("matches the snapshot", () => {
+jest.mock("@/utils/helpers/useWindowDimensions");
+
+describe("Tunnellers Component", () => {
+  const renderWithMockedDimensions = (width: number, height: number) => {
+    (useWindowDimensionsHook.useWindowDimensions as jest.Mock).mockReturnValue({
+      width,
+      height,
+    });
+
+    return render(<Tunnellers />);
+  };
+
+  beforeEach(() => {
+    (useWindowDimensionsHook.useWindowDimensions as jest.Mock).mockReturnValue({
+      width: 1024,
+      height: 768,
+    });
+  });
+
+  it("matches snapshot", () => {
     const { asFragment } = render(<Tunnellers />);
-
     expect(asFragment()).toMatchSnapshot();
   });
 
-  test("renders link to all Tunnellers page", () => {
+  it("renders link to all Tunnellers page", () => {
     render(<Tunnellers />);
-
     const tunnellersLink = screen.getByRole("link", {
       name: "Discover The New Zealand Tunnellers",
     });
     expect(tunnellersLink).toHaveAttribute("href", "/tunnellers");
+  });
+
+  it("renders SVG with text for width <= 512", () => {
+    const { container } = renderWithMockedDimensions(500, 800);
+    const svgElement = container.querySelector("svg");
+    expect(svgElement).toBeInTheDocument();
+    expect(svgElement).toHaveAttribute("viewBox", "0 0 100 125");
+
+    const textElement = container.querySelector("text");
+    expect(textElement).toBeInTheDocument();
+
+    const tspanElements = container.querySelectorAll("tspan");
+    expect(tspanElements.length).toBe(7);
+    expect(tspanElements[0].textContent).toBe("The Kiwis");
+    expect(tspanElements[1].textContent).toBe("who");
+    expect(tspanElements[2].textContent).toBe("fought");
+    expect(tspanElements[3].textContent).toBe("beneath");
+    expect(tspanElements[4].textContent).toBe("the no");
+    expect(tspanElements[5].textContent).toBe("man’s");
+    expect(tspanElements[6].textContent).toBe("land");
+  });
+
+  it("renders SVG with text for width > 512", () => {
+    const { container } = renderWithMockedDimensions(600, 800);
+    const svgElement = container.querySelector("svg");
+    expect(svgElement).toBeInTheDocument();
+    expect(svgElement).toHaveAttribute("viewBox", "0 0 100 60");
+
+    const textElement = container.querySelector("text");
+    expect(textElement).toBeInTheDocument();
+
+    const tspanElements = container.querySelectorAll("tspan");
+    expect(tspanElements.length).toBe(4);
+    expect(tspanElements[0].textContent).toBe("The Kiwis");
+    expect(tspanElements[1].textContent).toBe("who fought");
+    expect(tspanElements[2].textContent).toBe("beneath the");
+    expect(tspanElements[3].textContent).toBe("no man’s land");
   });
 });
