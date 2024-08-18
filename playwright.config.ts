fcd8e7df303b02ecbc54ac4a7a27f0f41dd0fc46
ByAnthony <anthony.byledbal@icloud.com>
2024-08-18 17:28:58 +0100

    add env variables to playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,6 +10,10 @@ import { defineConfig, devices } from "@playwright/test";
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
+
+const PORT = process.env.PORT || 3000;
+const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
+
 export default defineConfig({
   testDir: "./__tests__/e2e",
   /* Run tests in files in parallel */
@@ -25,7 +29,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:3000",
+    baseURL: BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
