790bf1ecc1a64deea7ab9e781345d0850f265326
ByAnthony <anthony.byledbal@icloud.com>
2024-09-15 21:52:43 +0100

    add comments for history pages

--- app/history/beneath-artois-fields/page.tsx
+++ app/history/beneath-artois-fields/page.tsx
@@ -2,6 +2,8 @@ import { promises as fs } from "fs";
 
 import { Article } from "@/components/Article/Article";
 
+// Limitation from vercel compute time on the database
+// See call-to-pick-and-shovel/page.tsx for database fetching
 export default async function Page() {
   const file = await fs.readFile(
     process.cwd() + "/utils/data/json/history/beneath-artois-fields.json",
