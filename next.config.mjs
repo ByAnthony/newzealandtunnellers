2d4597cd53415b0db60b55c45f33049866d25795
Anthony Byledbal <anthony.byledbal@skyscanner.net>
2024-09-20 19:40:08 +0100

    prod testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
+  // Temporary for prod testing
+  basePath: "/staging",
+  assetPrefix: "/staging",
+  experimental: {
+    // This is experimental but can
+    // be enabled to allow parallel threads
+    // with nextjs automatic static generation
+    workerThreads: false,
+    cpus: 1,
+  },
 };
 
 export default nextConfig;
