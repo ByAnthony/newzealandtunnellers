8e52aa19b50d101fa09d9677629731d53fa82734
ByAnthony <anthony.byledbal@icloud.com>
2024-11-29 21:31:40 +0000

    increase connection limit

--- __tests__/unit/utils/database/mysqlConnection.test.ts
+++ __tests__/unit/utils/database/mysqlConnection.test.ts
@@ -28,7 +28,7 @@ describe("mysqlConnection", () => {
     await mysqlConnection.getConnection();
 
     expect(mysql.createPool).toHaveBeenCalledWith({
-      connectionLimit: 10,
+      connectionLimit: 25,
       host: "localhost",
       user: "user",
       password: "password",
--- utils/database/mysqlConnection.ts
+++ utils/database/mysqlConnection.ts
@@ -1,7 +1,7 @@
 import mysql from "mysql2/promise";
 
 export const mysqlConnection = mysql.createPool({
-  connectionLimit: 10,
+  connectionLimit: 25,
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
